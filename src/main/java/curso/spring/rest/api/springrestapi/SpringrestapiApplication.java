cc894d2301926d8090d174c525ca4595efe53827
Guilherme Magno <guilherme.mprata@gmail.com>
2022-08-28 11:02:38 -0300

    config cors

--- src/main/java/curso/spring/rest/api/springrestapi/SpringrestapiApplication.java
+++ src/main/java/curso/spring/rest/api/springrestapi/SpringrestapiApplication.java
@@ -4,18 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableTransactionManagement
 @RestController
 @EnableAutoConfiguration
-public class SpringrestapiApplication {
+public class SpringrestapiApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(SpringrestapiApplication.class, args);
 	}
 
+	// mapeamento global que reflete em todo o sistema
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/usuario/**")
+				.allowedMethods("*")
+				.allowedOrigins("*");
+//				.allowedMethods("POST", "PUT", "GET", "DELETE")
+//				.allowedOrigins("localhost:8080");
+	}
 }
---
+++ src/main/java/curso/spring/rest/api/springrestapi/controller/IndexController2.java
@@ -0,0 +1,79 @@
+package curso.spring.rest.api.springrestapi.controller;
+
+import curso.spring.rest.api.springrestapi.model.Usuario;
+import curso.spring.rest.api.springrestapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value = "/usuario2")
+public class IndexController2 {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable(value = "id") Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        return new ResponseEntity(usuario.get(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity<List<Usuario>> buscarTodosUsuarios() {
+        List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+        return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+
+        for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+            usuario.getTelefones().get(pos).setUsuario(usuario);
+        }
+
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+        return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Usuario> atualizarUsuario(@RequestBody Usuario usuario) {
+
+        for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+            usuario.getTelefones().get(pos).setUsuario(usuario);
+        }
+
+        Usuario usuarioAtualizado = usuarioRepository.save(usuario);
+
+        return new ResponseEntity<>(usuarioAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = "application/text")
+    public String deletarUsuario(@PathVariable("id") Long id) {
+        usuarioRepository.deleteById(id);
+        return "OK";
+    }
+
+   /*  @PutMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+    public ResponseEntity atualizarVenda(@PathVariable Long iduser, @PathVariable Long idvenda) {
+//        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        return new ResponseEntity("Venda atualizada", HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+    public ResponseEntity cadastrarVenda(@PathVariable Long iduser, @PathVariable Long idvenda) {
+
+        // Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        return new ResponseEntity("iduser: " + iduser + "idvenda: " + idvenda, HttpStatus.OK);
+    }
+    */
+}
